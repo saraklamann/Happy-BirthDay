2c59f372e1f54929095fe34bc3547ff308c60f44
Sara Caroline Klamann Jucoski <119754784+saraklamann@users.noreply.github.com>
2023-09-04 14:51:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('redirect-button').addEventListener('click', function () {
+    window.location.href = 'https://www.notion.so/Cartinha-de-anivers-rio-Cassia-3-e5db2497290a4be2bfaac9a2ff018a78';
+});
